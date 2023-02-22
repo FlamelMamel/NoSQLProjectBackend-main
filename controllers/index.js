6434fef8d6b9ef9cbbf9f655d9f97637a8ac533c
Flamel <flamelmamel@gmail.com>
2023-02-23 01:52:51 +0600

    gg

--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-export * as DoctorController from './DoctorController.js';
-export * as UserController from './UserController.js';
-export * as AdminController from './AdminController.js';
-export * as AppointmentController from './AppointmentController.js';
-export * as HospitalController from './HospitalController.js';
--- index.js
+++ index.js
@@ -2,17 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { registerValidator, loginValidator } from './validators/auth.js';
-import { addDoctorValidator } from './validators/doctor.js';
-import { adminValidator } from './validators/admin.js';
 import checkUser from './middlewares/checkUser.js';
-import checkAdmin from './middlewares/checkAdmin.js';
-import { UserController, DoctorController, AdminController, AppointmentController, HospitalController } from './controllers/index.js';
-import checkDoctor from './middlewares/checkDoctor.js';
+
+import UserController from './controllers/UserController.js';
+
 import multer from 'multer';
 
 mongoose.set("strictQuery", false);
 mongoose.connect(
-     'mongodb+srv://admin:fDJOm5IhWXOwd0Hp@cluster0.5cekcxl.mongodb.net/hospitalDB?retryWrites=true&w=majority'
+     'mongodb+srv://Flamel:jvY26UBU6i0r5pig@cluster0.8mp3abp.mongodb.net/?retryWrites=true&w=majority'
 ).then(() => {
      console.log('DB connected');
 }).catch((err) => {
@@ -44,34 +42,9 @@ app.get('/', async(req, res) => {
 app.post('/user/register', registerValidator, UserController.register); 
 app.post('/user/login', loginValidator, UserController.login);
 app.get('/user/myprofile', checkUser, UserController.getMyProfile);
-app.post('/user/appointment', checkUser, AppointmentController.bookAppointment);
 app.post('/user/update', checkUser, UserController.updateUserProfile);
 
-app.get('/doctors', checkUser, DoctorController.getAllDoctors);
-app.post('/doctor/register', addDoctorValidator, DoctorController.register);
-app.post('/doctor/login', loginValidator, DoctorController.login);
-app.get('/doctor/myprofile', checkDoctor, DoctorController.getMyProfile);
-app.post('/doctor/appointment', checkDoctor, AppointmentController.add);
-app.post('/doctor/upload/profilepicture', checkDoctor, upload.single('docimg'), DoctorController.uploadProfilePicture);
-app.post('/doctor/upload/cv', checkDoctor, upload.single('cv'), DoctorController.uploadCV);
-app.post('/doctor/delete/profilepicture', checkDoctor, DoctorController.deleteProfilePicture);
-app.get('/doctor/appointments/notstarted', checkDoctor, DoctorController.getAllMyNotStartedAppointments);
-
-app.get('/hospitals', HospitalController.getAllHospitals);
-app.get('/hospitals/cities', HospitalController.getAllCitiesForSorting);
-app.get('/hospitals/:city', HospitalController.getAllHospitalsFromCity);
-
-app.post('/admin/register', adminValidator, AdminController.register);
-app.post('/admin/login', adminValidator, AdminController.login);
-app.post('/admin/doctor/confirm', checkAdmin, AdminController.confirm);
-app.post('/admin/doctor/deny', checkAdmin, AdminController.deny);
-app.get('/admin/doctors', checkAdmin, AdminController.getAllDoctorsWithoutAccess);
-app.get('/admin/hospitals/noinfo', checkAdmin, AdminController.getAllHospitalsWithoutInfo);
-app.post('/admin/hospital/update', checkAdmin, AdminController.updateHospitalInfo);
-app.post('/admin/hospital/upload', checkAdmin, upload.single('hospitalImg'), AdminController.uploadHospitalImage);
-
-
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 
 app.listen(port, (err) => {
      if(err){
